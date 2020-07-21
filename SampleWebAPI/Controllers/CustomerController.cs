815f6cedf6770bbad5118a602555d8ac48563902
unknown <ptvce@yahoo.com>
2020-07-21 17:15:15 +0430

    init web api sample with swagger

---
+++ SampleWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleWebAPI.Data;
+using SampleWebAPI.Entites;
+
+namespace SampleWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private AdventureWorkSampleDBContext _context;
+        public CustomerController(AdventureWorkSampleDBContext context)
+        {
+            _context = context;
+        }
+
+        //http://localhost/api/customer
+        /// <summary>
+        /// لیست مشتریان سازمان
+        /// </summary>
+        /// <param name="page">صفحه جاری</param>
+        /// <param name="limit">تعداد در هر صفحه</param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> Get(int page =1, int limit = 10)
+        {
+            int skip = (page - 1) * limit;
+            var result = _context.Customer.OrderBy(q => q.CustomerId).Skip(skip).Take(limit).ToListAsync();
+            return Ok(result);
+        }
+        /// <summary>
+        /// اطلاعات مشتریان بر اساس کد مشتری
+        /// </summary>
+        /// <param name="id">کد مشتری</param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = _context.Customer.Find(id);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// افزودن مشتری جدید
+        /// </summary>
+        /// <param name="model">اطلاعات مشتری</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Customer model)    //FromBody : باعث می شود که به جز بادی از روش های دیگر نتواند دیتا ارسال کند مثلا با کوئری استرینگ نتواند
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("اطلاعات ارسالی صحیح نمی باشد");
+            await _context.Customer.AddAsync(model);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ SampleWebAPI/Startup.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using SampleWebAPI.Data;
+
+namespace SampleWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AdventureWorkSampleDBContext>(option => option.UseSqlServer(Configuration.GetConnectionString("AdventureWorkSampleDBConnectionString")));
+
+            #region Swagger
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "VANDA Sample API",
+                    Description = "Sample System - Version01",
+                    TermsOfService = new Uri("http://vanda.net"),
+                    License = new OpenApiLicense
+                    {
+                        Name = "Vanda",
+                        Url = new Uri("http://vanda.net/"),
+                    }
+                });
+
+                // Swagger 2.+ support
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "Bearer",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            #endregion
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            #region Swagger
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            app.UseCors("FadApp");
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FAD API V1");
+            });
+
+
+            #endregion
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
